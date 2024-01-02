d2dade4dd08f3092b3da0e445eeee19ddfd03ade
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-01-02 23:08:47 +0200

    Update backend-service.js
    upp URL

--- src/js/backend-service.js
+++ src/js/backend-service.js
@@ -5,20 +5,21 @@ export class NewApiService {
     this.KEY = '41531809-f9219a766117007ff116a3463';
     this.searchQuery = '';
     this.page = 1;
+    this.itemCount = 40;
+    this.orientation = 'horizontal';
+    this.imageType = 'photo';
   }
 
   async getPhoto() {
     try {
       const response = await fetch(
-        `https://pixabay.com/api/?key=${this.KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&per_page=40&page=${this.page}`
+        `https://pixabay.com/api/?key=${this.KEY}&q=${this.searchQuery}&image_type=${this.imageType}&orientation=${this.orientation}&per_page=${this.itemCount}&page=${this.page}`
       );
 
       if (!response.ok) {
         throw new Error(response.status);
       }
-
       const images = await response.json();
-
       this.incrementPage();
       return images;
     } catch (error) {
