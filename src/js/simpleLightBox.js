b53a61e93a80f78b406ce526723065afc60f8977
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-01-05 13:33:18 +0200

    Update simpleLightBox.js

--- src/js/simpleLightBox.js
+++ src/js/simpleLightBox.js
@@ -2,8 +2,9 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export function initializeLightbox() {
-  new SimpleLightbox('.gallery a', {
+  const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
   });
+  lightbox.refresh();
 }
